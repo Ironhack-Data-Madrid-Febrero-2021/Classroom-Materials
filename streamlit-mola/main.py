d99e0fb1c6e0ec3ecb67d3730ea94eb2d30e7fb6
agalvezcorell <agalvezcorell@gmail.com>
2021-05-14 20:25:29 +0200

    updating

--- streamlit-mola/main.py
+++ streamlit-mola/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
-import pandas as pd
 import src.manage_data as dat
+import plotly.express as px
 
 imagen = Image.open("images/portada2.jpg")
 
@@ -12,7 +12,27 @@ st.write("""
 # Mi súper app 
 Con Jake el perro y Finn el humano lo pasaremos guay 🚀
 """)
-
+#dat es el alias y carga_data es la función
 datos = dat.carga_data()
 
+#st.dataframe visualiza un dataframe
 st.dataframe(datos)
+
+#datos_grafico llama a la función que genera el gráfico de barras
+datos_grafico = dat.grafico_barras_st()
+st.dataframe(datos_grafico)
+
+st.bar_chart(datos_grafico)
+lista = dat.lista_personajes()
+
+personaje = st.selectbox("Selecciona algo de la lista", lista)
+st.write("Has elegido", personaje)
+
+
+datagraf = dat.grafico(personaje)
+
+fig = px.line(datagraf, y="polarity", title=f"Evolución de la polaridad de  {personaje}")
+
+st.plotly_chart(fig)
+
+
--- streamlit-mola/src/manage_data.py
+++ streamlit-mola/src/manage_data.py
@@ -5,3 +5,18 @@ def carga_data():
     data = pd.read_csv("data/clean.csv")
     return data
 
+def grafico_barras_st():
+    data = carga_data()
+    data = data.groupby("character_name").agg({"character_name":'count'}).rename(columns={"character_name":"character_name", "character_name":"número de frases"}).reset_index().set_index("character_name", drop=True)
+    return data
+
+
+def lista_personajes():
+    data = carga_data()
+    return list(data.character_name.unique())
+
+
+def grafico(person):
+    data = carga_data()
+    data = data[(data["character_name"] == f"{person}")]
+    return data
